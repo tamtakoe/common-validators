c4b49b4673bdb56fa4a89d6d90ac5bb001162936
tamtakoe <tamtakoe@gmail.com>
2016-06-22 22:40:43 +0300

    validators-constructor was updated

--- dist/common-validators.js
+++ dist/common-validators.js
@@ -766,7 +766,7 @@ Validators.prototype.add = function (name, validator) {
             var options = void 0;
             var args = Array.prototype.slice.call(arguments, 2);
 
-            if ({}.toString.call(arguments[1]) === '[object Object]') {
+            if (arguments[1] === undefined || {}.toString.call(arguments[1]) === '[object Object]') {
                 options = arguments[1] || {};
             } else {
                 options = arguments[2] || {};
